4146e0d75b31f1fb6b205c01048f202bc47ee8a3
SirKiboro <paulkiboro58@gmail.com>
2026-04-08 09:02:41 +0300

    Initial commit

---
+++ src/main/java/com/malihub/MaliHubApplication.java
@@ -0,0 +1,13 @@
+package com.malihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaliHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaliHubApplication.class, args);
+	}
+
+}
